6388bbef1d36b2a8ede3995bbaa3fa315898b632
daid <daid303@gmail.com>
2020-12-06 22:38:04 +0100

    Switch to mGBA-SDL, maybe that does not crash on github actions.

--- emulators/mgba.py
+++ emulators/mgba.py
@@ -10,13 +10,7 @@ class MGBA(Emulator):
     def setup(self):
         download("https://github.com/mgba-emu/mgba/releases/download/0.8.4/mGBA-0.8.4-win32.7z", "downloads/mgba.7z")
         extract("downloads/mgba.7z", "emu/mgba")
-        setDPIScaling("emu/mgba/mGBA-0.8.4-win32/mGBA.exe")
+        setDPIScaling("emu/mgba/mGBA-0.8.4-win32/mgba-sdl.exe")
     
     def startProcess(self, rom, *, gbc=False):
-        return subprocess.Popen(["emu/mgba/mGBA-0.8.4-win32/mGBA.exe", "-1", os.path.abspath(rom)], cwd="emu/mgba/mGBA-0.8.4-win32/")
-
-    def getScreenshot(self):
-        screenshot = getScreenshot(self.title_check)
-        if screenshot is None:
-            return None
-        return screenshot.crop((0, screenshot.size[1] - 144, 160, screenshot.size[1]))
+        return subprocess.Popen(["emu/mgba/mGBA-0.8.4-win32/mgba-sdl.exe", "-1", os.path.abspath(rom)], cwd="emu/mgba/mGBA-0.8.4-win32/")
