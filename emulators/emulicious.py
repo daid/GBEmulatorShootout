ce59f6aa9ba967f305a9a941a37434bfeb93cdc5
daid <daid303@gmail.com>
2020-12-07 18:17:29 +0100

    Run emulious in turbo mode, and add boot roms

--- emulators/emulicious.py
+++ emulators/emulicious.py
@@ -12,9 +12,11 @@ class Emulicious(Emulator):
         extract("downloads/Emulicious.zip", "emu/emulicious")
         setDPIScaling("emu/emulicious/Emulicious.exe")
         shutil.copyfile(os.path.join(os.path.dirname(__file__), "emulicious.ini"), "emu/emulicious/Emulicious.ini")
+        download("https://gbdev.gg8.se/files/roms/bootroms/cgb_bios.bin", "emu/emulicious/cgb_boot.bin")
+        download("https://gbdev.gg8.se/files/roms/bootroms/dmg_boot.bin", "emu/emulicious/dmg_boot.bin")
 
     def startProcess(self, rom, *, gbc=False):
-        return subprocess.Popen(["java", "-jar", "Emulicious.jar", os.path.abspath(rom)], cwd="emu/emulicious")
+        return subprocess.Popen(["java", "-jar", "Emulicious.jar", "-throttle", "10000", os.path.abspath(rom)], cwd="emu/emulicious")
 
     def getScreenshot(self):
         screenshot = getScreenshot(self.title_check)
