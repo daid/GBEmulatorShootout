bee52caf3ebf0da2da13453141d9f78e475fb5be
daid <daid303@gmail.com>
2020-12-07 10:11:41 +0100

    Wrong target path for mgba ini file...

--- emulators/mgba.py
+++ emulators/mgba.py
@@ -12,7 +12,7 @@ class MGBA(Emulator):
         download("https://github.com/mgba-emu/mgba/releases/download/0.8.4/mGBA-0.8.4-win32.7z", "downloads/mgba.7z")
         extract("downloads/mgba.7z", "emu/mgba")
         setDPIScaling("emu/mgba/mGBA-0.8.4-win32/mGBA.exe")
-        shutil.copyfile(os.path.join(os.path.dirname(__file__), "mgba.qt.ini"), "emu/mGBA-0.8.4-win32/qt.ini")
+        shutil.copyfile(os.path.join(os.path.dirname(__file__), "mgba.qt.ini"), "emu/mgba/mGBA-0.8.4-win32/qt.ini")
     
     def startProcess(self, rom, *, gbc=False):
         return subprocess.Popen(["emu/mgba/mGBA-0.8.4-win32/mGBA.exe", "-1", os.path.abspath(rom)], cwd="emu/mgba/mGBA-0.8.4-win32/")
