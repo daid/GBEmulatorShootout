cd3aa830eeafbb715a48ec6190767f792ff5a5ec
daid <daid303@gmail.com>
2020-12-07 10:52:33 +0100

    Run bgb on proper gbc or dmg mode. Run no$dmg in black and white mode instead of lcd colors

--- emulators/bgb.py
+++ emulators/bgb.py
@@ -15,4 +15,4 @@ class BGB(Emulator):
         shutil.copyfile(os.path.join(os.path.dirname(__file__), "bgb.ini"), "emu/bgb/bgb.ini")
     
     def startProcess(self, rom, *, gbc=False):
-        return subprocess.Popen(["emu/bgb/bgb.exe", "-set", "Speed=%g" % (self.speed), "-set", "LoadRomWarnings=0", "-set", "Width=160", "-set", "Height=144", "-set", "SoundOut=null", os.path.abspath(rom)], cwd="emu/bgb")
+        return subprocess.Popen(["emu/bgb/bgb.exe", "-set", "SystemMode=%d" % (1 if gbc else 0), "-set", "Speed=%g" % (self.speed), "-set", "LoadRomWarnings=0", "-set", "Width=160", "-set", "Height=144", "-set", "SoundOut=null", os.path.abspath(rom)], cwd="emu/bgb")
