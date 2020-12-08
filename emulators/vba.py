ca5997c573066a75c01dcc47a96229be2c6fcb02
daid <daid303@gmail.com>
2020-12-08 14:27:50 +0100

    Fix running vba-m in proper required mode

--- emulators/vba.py
+++ emulators/vba.py
@@ -28,11 +28,14 @@ class VBAM(Emulator):
         downloadGithubRelease("visualboyadvance-m/visualboyadvance-m", "downloads/vba-m.zip", filter=lambda n: "Win" in n and "64bit" in n and n.endswith(".zip"))
         extract("downloads/vba-m.zip", "emu/vba-m")
         setDPIScaling("emu/vba-m/visualboyadvance-m.exe")
-        shutil.copyfile(os.path.join(os.path.dirname(__file__), "vbam.ini"), "emu/vba-m/vbam.ini")
         download("https://gbdev.gg8.se/files/roms/bootroms/cgb_bios.bin", "emu/vba-m/cgb_bios.bin")
         download("https://gbdev.gg8.se/files/roms/bootroms/dmg_boot.bin", "emu/vba-m/dmg_boot.bin")
 
     def startProcess(self, rom, *, gbc=False):
+        if gbc:
+            shutil.copyfile(os.path.join(os.path.dirname(__file__), "vbam.gbc.ini"), "emu/vba-m/vbam.ini")
+        else:
+            shutil.copyfile(os.path.join(os.path.dirname(__file__), "vbam.dmg.ini"), "emu/vba-m/vbam.ini")
         return subprocess.Popen(["emu/vba-m/visualboyadvance-m.exe", os.path.abspath(rom)], cwd="emu/vba-m")
 
     def getScreenshot(self):
