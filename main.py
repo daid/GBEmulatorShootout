fe627db7add683bdd725299183337e272acba455
daid <daid303@gmail.com>
2020-12-11 18:21:58 +0100

    Fix total counts and sorting

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
         results[emulator] = {}
         for test in tests:
             results[emulator][test] = emulator.run(test)
-    emulators.sort(key=lambda emulator: len([result[0] for result in results[emulator].values() if result[0]]), reverse=True)
+    emulators.sort(key=lambda emulator: len([result[0] for result in results[emulator].values() if result.result == "PASS"]), reverse=True)
     
     for emulator in emulators:
         data = {str(test): {'result': result.result, 'startuptime': result.startuptime, 'runtime': result.runtime, 'screenshot': imageToBase64(result.screenshot)} for test, result in results[emulator].items()}
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     f.write("<html><head><style>table { border-collapse: collapse } td, th { border: #333 solid 1px; text-align: center; line-height: 1.5} .PASS { background-color: #6e2 } .FAIL { background-color: #e44 } .UNKNOWN { background-color: #fd6 } td{font-size:80%} th{background:#eee} th:first-child{text-align:right; padding-right:4px} body{font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif} </style></head><body><table>\n")
     f.write("<tr><th>-</th>\n")
     for emulator in emulators:
-        passed = len([result[0] for result in results[emulator].values() if result[0]])
+        passed = len([result for result in results[emulator].values() if result.result == "PASS"])
         f.write("  <th>%s (%d/%d)</th>\n" % (emulator, passed, len(results[emulator])))
     f.write("</tr>\n");
     for test in tests:
