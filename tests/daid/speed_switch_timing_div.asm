1e5736055727e1230ffde071ce89f791ef3d5176
daid <daid303@gmail.com>
2020-12-11 22:06:12 +0100

    Fix the speed switch test roms, which where giving the wrong results

--- tests/daid/speed_switch_timing_div.asm
+++ tests/daid/speed_switch_timing_div.asm
@@ -4,14 +4,17 @@ TEST_SIZE equ 32
 INCLUDE "speed_switch_timing.inc"
 
 label:
-  db "DIV:"
+  db "DIV:", 0
+
+preSwitch:
+  ; We wait till DIV is $40, to we can see very well that DIV gets reset on a speed toggle.
+  ld a, [rDIV]
+  cp $40
+  ret z
+  jr preSwitch
 
 expect:
   db $00, $00, $00, $00, $00, $00, $00, $00
-  db $00, $00, $00, $01, $01, $01, $01, $01
+  db $00, $00, $00, $00, $01, $01, $01, $01
   db $01, $01, $01, $01, $01, $01, $01, $01
-  db $02, $02, $02, $02, $02, $02, $02, $02
-  db $02, $02, $02, $02, $02, $03, $03, $03
-  db $03, $03, $03, $03, $03, $03, $03, $03
-  db $03, $03, $04, $04, $04, $04, $04, $04
-  db $04, $04, $04, $04, $04, $04, $04, $05
+  db $01, $02, $02, $02, $02, $02, $02, $02
