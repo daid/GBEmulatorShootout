a3a0ccd58328b4a6f24ebb35c0cc5d160b0da528
daid <daid303@gmail.com>
2020-12-11 19:22:50 +0100

    Give the RTC tests some more time

--- tests/ax6.py
+++ tests/ax6.py
@@ -2,7 +2,7 @@ from test import Test
 
 
 all = [
-    Test("ax6/rtc3test-1.gb", runtime=2.5, gbc=True),
+    Test("ax6/rtc3test-1.gb", runtime=5.5, gbc=True),
     Test("ax6/rtc3test-2.gb", runtime=1.5, gbc=True),
-    Test("ax6/rtc3test-3.gb", runtime=13.0, gbc=True),
+    Test("ax6/rtc3test-3.gb", runtime=20.0, gbc=True),
 ]
