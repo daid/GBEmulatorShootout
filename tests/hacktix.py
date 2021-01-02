ae4b64a0236a5ea48ef882f95960472aeb7ae7b5
Daid <daid303@gmail.com>
2021-01-02 10:25:53 +0100

    Run bully in both dmg and gbc

--- tests/hacktix.py
+++ tests/hacktix.py
@@ -2,6 +2,8 @@ from test import Test
 
 
 all = [
-    Test("hacktix/bully.gb", runtime=0.5,
+    Test("hacktix/bully.gb (DMG)", rom="hacktix/bully.gb", runtime=0.5,
+        description="DMA bus conflict to execute code.", url="https://github.com/Hacktix/BullyGB"),
+    Test("hacktix/bully.gb (GBC)", rom="hacktix/bully.gb", runtime=0.5, gbc=True,
         description="DMA bus conflict to execute code.", url="https://github.com/Hacktix/BullyGB"),
 ]
