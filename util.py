5192e3179ddd794ab9eb641c8e21f9e57ed2e32d
Daid <daid303@gmail.com>
2021-02-05 20:58:47 +0100

    Only fake the user agent for no$dmg, else vba fails to download

--- util.py
+++ util.py
@@ -16,7 +16,7 @@ def download(url, filename, fake_headers=False):
         print("Downloading %s" % (url))
         headers = {}
         if fake_headers:
-            headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36',
+            headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36'
         r = requests.get(url, allow_redirects=True, headers=headers)
         open(filename, "wb").write(r.content)
 
