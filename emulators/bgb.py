9d69b103b79d42553a3203d2226ca313604e6155
Daid <daid303@gmail.com>
2021-04-21 20:07:03 +0200

    Set detectgba flag

--- emulators/bgb.py
+++ emulators/bgb.py
@@ -32,4 +32,4 @@ class BGBRC(Emulator):
         shutil.copyfile(os.path.join(os.path.dirname(__file__), "bgb.ini"), "emu/bgb/bgb.ini")
     
     def startProcess(self, rom, *, gbc=False):
-        return subprocess.Popen(["emu/bgb/bgb-159-rc64.exe", "-set", "SystemMode=%d" % (1 if gbc else 0), "-set", "Speed=%g" % (self.speed), "-set", "LoadRomWarnings=0", "-set", "Width=160", "-set", "Height=144", "-set", "SoundOut=null", os.path.abspath(rom)], cwd="emu/bgb")
+        return subprocess.Popen(["emu/bgb/bgb-159-rc64.exe", "-set", "SystemMode=%d" % (1 if gbc else 0), "-set", "Speed=%g" % (self.speed), "-set", "LoadRomWarnings=0", "-set", "Width=160", "-set", "detectgba=1", "-set", "Height=144", "-set", "SoundOut=null", os.path.abspath(rom)], cwd="emu/bgb")
