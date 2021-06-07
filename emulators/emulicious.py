b4ba2d126d10507c20b3678c2bdb2cd4d72c092b
Calindro <info@emulicious.net>
2021-06-07 13:47:55 +0200

    Improved Emulicious configuration
    - Use integer scaling
    - Disable color correction
    - Fixed download link

--- emulators/emulicious.py
+++ emulators/emulicious.py
@@ -10,11 +10,8 @@ class Emulicious(Emulator):
         super().__init__("Emulicious", "https://emulicious.net/", startup_time=1.0)
     
     def setup(self):
-        res = requests.get("https://emulicious.net/downloads/")
-        url = re.search("data-downloadurl=\"([^\"]+)\"", res.text).group(1)
-        download(url, "downloads/Emulicious.zip")
+        download("https://emulicious.net/download/emulicious/?wpdmdl=205", "downloads/Emulicious.zip")
         extract("downloads/Emulicious.zip", "emu/emulicious")
-        setDPIScaling("emu/emulicious/Emulicious.exe")
         download("https://gbdev.gg8.se/files/roms/bootroms/cgb_boot.bin", "emu/emulicious/cgb_boot.bin")
         download("https://gbdev.gg8.se/files/roms/bootroms/dmg_boot.bin", "emu/emulicious/dmg_boot.bin")
 
