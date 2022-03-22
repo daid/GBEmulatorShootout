835434af15c6cbd5e094f9f19e056ef991285fc0
Luke Usher <luke.usher@outlook.com>
2022-03-22 23:06:32 +0000

    add ares to test suite (dmg only for now)

---
+++ emulators/ares.py
@@ -0,0 +1,47 @@
+from util import *
+from emulator import Emulator
+from test import *
+import glob
+import shutil
+import os
+import PIL.Image
+import PIL.ImageOps
+from distutils.dir_util import copy_tree
+
+class Ares(Emulator):
+    def __init__(self):
+        super().__init__("ares", "https://ares-emu.net/", startup_time=2.2)
+        self.title_check = lambda title: "ares" in title
+
+    def setup(self):
+        downloadGithubRelease("ares-emulator/ares", "downloads/ares.zip", filter=lambda n: "ares" in n and "windows" in n and n.endswith(".zip"), allow_prerelease=True)
+        extract("downloads/ares.zip", "emu/ares")
+
+        if not os.path.exists("emu/ares/ares.exe"):
+          copy_tree(os.path.join("emu/ares", os.listdir("emu/ares")[0]), "emu/ares")
+
+        setDPIScaling("emu/ares/ares.exe")
+        shutil.copyfile(os.path.join(os.path.dirname(__file__), "ares-settings.bml"), "emu/ares/settings.bml")
+
+    def startProcess(self, rom, *, model, required_features):
+        # Temporarily disable non-dmg targets, startup-time-test doesn't work in cgb yet
+        if model != DMG:
+            return None
+
+        target = "emu/ares/ares-rom.gb"
+        self.cgb = model == CGB
+        if self.cgb:
+            target += "c"
+        shutil.copy(rom, target)
+        return subprocess.Popen(["emu/ares/ares.exe", os.path.abspath(target)], cwd="emu/ares")
+
+    def getScreenshot(self):       
+        screenshot = getScreenshot(self.title_check)
+        if screenshot is None:
+            return None
+        screenshot = screenshot.reduce(3)
+        if not self.cgb:        
+            screenshot = screenshot.convert(mode="L", dither=PIL.Image.NONE)
+            screenshot = PIL.ImageOps.autocontrast(screenshot)
+            screenshot.save("tmp.png")
+        return screenshot
