7c3fc900c345c3e30ce5c133f9811d0601dc67e0
Docheinstein <dottorestefano97@gmail.com>
2024-04-16 22:28:52 +0200

    Run ax6 RTC test also on DMG
    ax6's RTC tests do not actually need CGB; DMG is fully capable of run
    these roms. Therefore, the CGB requirement has been removed and
    the screenshots of such roms have been updated with the DMG's one.

--- testroms/ax6.py
+++ testroms/ax6.py
@@ -2,7 +2,7 @@ from test import *
 
 
 all = [
-    Test("ax6/rtc3test-1.gb", runtime=9.5, model=CGB),
-    Test("ax6/rtc3test-2.gb", runtime=7.5, model=CGB),
-    Test("ax6/rtc3test-3.gb", runtime=20.0, model=CGB),
+    Test("ax6/rtc3test-1.gb", runtime=9.5),
+    Test("ax6/rtc3test-2.gb", runtime=7.5),
+    Test("ax6/rtc3test-3.gb", runtime=20.0),
 ]
